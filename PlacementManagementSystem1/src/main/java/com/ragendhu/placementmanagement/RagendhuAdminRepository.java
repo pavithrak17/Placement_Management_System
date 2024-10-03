6112e6ff19433ee51f7db9dca60c1d4a939b2438
PAVITHRA K <pavithrak1743@gmail.com>
2024-10-03 12:09:18 +0530

    commit

--- PlacementManagementSystem1/src/main/java/com/ragendhu/placementmanagement/RagendhuAdminRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ragendhu.placementmanagement;
-import org.springframework.data.jpa.repository.JpaRepository;
-/*JPARepository is exists in Spring Data JPA dependencies and it contains
- * all the CRUD operation of Spring JPA
- */
-public interface RagendhuAdminRepository extends JpaRepository<RagendhuAdmin,Integer>{
-}

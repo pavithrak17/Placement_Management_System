0441af11fcccde16e49e84162929419905add6a9
PAVITHRA K <159021245+pavithrak17@users.noreply.github.com>
2024-10-22 23:45:00 +0530

    Updated PavithraUser.java

--- PlacementManagementSystem/src/main/java/com/pavithra/placementmanagement/PavithraUser.java
+++ PlacementManagementSystem/src/main/java/com/pavithra/placementmanagement/PavithraUser.java
@@ -1,91 +1,119 @@
 package com.pavithra.placementmanagement;
 
-	import jakarta.persistence.Entity;
-	import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+/**
+ * This is an entity class for the PavithraUser.
+ * It represents a user in the placement management system.
+ * Each instance of this class corresponds to a record in the database.
+ * @Entity: Marks the class as a JPA entity, 
+ * meaning it will be mapped to a database table.
+ */
+@Entity
+public class PavithraUser {
 	
-	/**
-	 * This is an entity class for the PavithraUser.
-	 * It represents a user in the placement management system.
-	 * Each instance of this class corresponds to a record in the database.
-	 * @Entity: Marks the class as a JPA entity, 
-	 * meaning it will be mapped to a database table.
-	 */
-	@Entity
-	public class PavithraUser {
-		
 	// Indicates the primary key field of the entity. (unique identifier for each user)
 	@Id
 	private Integer id;
 	// User's name
 	private String name;
-	// User type 
-	private Integer type;
+	// User type (e.g., admin, student, recruiter)
+	private String role;
 	// User's password for authentication
 	private String password;
-	
-	 /**
-     * Default constructor for PavithraUser.
-     * Required by JPA (Java Persistence API) for creating an empty object.
-     */
+	// User's email address
+	private String email;
+	// User's contact number
+	private String contactNumber;
+	// Status to track if the user is active/inactive
+	private boolean status;
+
+	/**
+	 * Default constructor for PavithraUser.
+	 * Required by JPA (Java Persistence API) for creating an empty object.
+	 */
 	public PavithraUser() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 	
-    /**
-     * Parameterized constructor for PavithraUser.
-     * Allows for creating an instance of PavithraUser with all properties. 
-     * id       the unique identifier for the user
-     * name     the name of the user
-     * type     the type of the user (role)
-     * password the user's password
-     */
-	public PavithraUser(Integer id, String name, Integer type, String password) {
+	/**
+	 * Parameterized constructor for PavithraUser.
+	 * Allows for creating an instance of PavithraUser with all properties.
+	 * @param id        the unique identifier for the user
+	 * @param name      the name of the user
+	 * @param role      the role/type of the user
+	 * @param password  the user's password
+	 * @param email     the user's email
+	 * @param contactNumber the user's contact number
+	 * @param status    the user's status (active/inactive)
+	 */
+	public PavithraUser(Integer id, String name, String role, String password, String email, String contactNumber, boolean status) {
 		super();
 		this.id = id;
 		this.name = name;
-		this.type = type;
+		this.role = role;
 		this.password = password;
+		this.email = email;
+		this.contactNumber = contactNumber;
+		this.status = status;
 	}
 	
-	//Getters/Setters: Allow for accessing and modifying the fields.
-	// Getter for 'id'
+	// Getters and Setters for the fields
+	
 	public Integer getId() {
 		return id;
 	}
 	
-	// Setter for 'id'
 	public void setId(Integer id) {
 		this.id = id;
 	}
 	
-	// Getter for 'name'
 	public String getName() {
 		return name;
 	}
 	
-	// Setter for 'name'
 	public void setName(String name) {
 		this.name = name;
 	}
 	
-	// Getter for 'type'
-	public Integer getType() {
-		return type;
+	public String getRole() {
+		return role;
 	}
 	
-	// Setter for 'type'
-	public void setType(Integer type) {
-		this.type = type;
+	public void setRole(String role) {
+		this.role = role;
 	}
 	
-	// Getter for 'password'
 	public String getPassword() {
 		return password;
 	}
 	
-	// Setter for 'password'
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getContactNumber() {
+		return contactNumber;
+	}
+	
+	public void setContactNumber(String contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+	
+	public boolean isStatus() {
+		return status;
+	}
+	
+	public void setStatus(boolean status) {
+		this.status = status;
 	}
 }
